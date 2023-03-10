518b5489d73ca5c4d13e12544507cb9de33cfd38
Aluel-Kuot <maryaluelkuot@gmail.com>
2023-03-10 19:58:37 +0300

    strings

---
+++ strings.js
@@ -0,0 +1,23 @@
+//quiz1 extacting chars
+let b="beautiful"
+let result=b.substring(0,4);
+console.log(result);
+
+//quiz2 inserting
+const food="I did not have appetite today"
+console.log()
+//quiz3
+const  story="She sells sea shells at the sea shore"
+console.log(indexof.the)
+//the
+console.log(count,"the");
+//s
+//quiz4
+let word="CONfidant";
+console.log(word.toUpperCase())
+let speak="amazing"
+const string1="The lady went to the market with her sister"
+console.log(string1.find)
+
+
+
